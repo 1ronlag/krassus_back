017f2e8e89602b935d795596c9089cb6dca9353f
1ronlag <nico.saavedrap@gmail.com>
2023-04-04 12:41:11 -0400

    data0404

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const { userRegiter, getProfiles, updateUsertById, deleteUserById} = require('../controllers/usersController')
-const { isLogin} = require ('../middlewares/isLogin')
+// const { isLogin} = require ('../middlewares/isLogin')
 const { reportRequest } = require('../middlewares/logger')
 
 router.post('/users', reportRequest, userRegiter )
-router.get('/users', reportRequest, isLogin, getProfiles)
+router.get('/users', reportRequest, getProfiles)
 router.put('/users/:id', reportRequest, updateUsertById)
 router.delete('/users/:id', reportRequest, deleteUserById)
 
